e4e1769021db85cb053da3cbd2a353f282ac3d3d
khoatranrb <khoamata20002@gmail.com>
2019-12-21 16:07:58 +0700

    update

---
+++ get_message.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/get_message', methods = ['POST'])
+
+def get_message():
+    message = request.get_json(force=True)
+    name = message['name']
+    response = {
+        'greeting': 'Message: '+name+' !'
+    }
+    return jsonify(response)
+
+app.run(debug=True)
